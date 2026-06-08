4495d7f73c7904846c61fb708ef50fdc56fd90fc
Dona_01 <52212725@ivt.edu.ar>
2026-06-08 18:42:05 -0300

    cambios.js

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,33 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado 
-
-resultadp = numero1 + numero2;
-
-console.log(resultado);
-alert(resultado)
+let suma
+let resta
+let multiplicacion
+let division
+suma = (30+30)
+resta = (30-20)
+multiplicacion = (20*6)
+division = (6/2)
+alert(suma)
+alert(resta)
+console.log(multiplicacion)
+console.log(division)
+alert("El resultado en la suma es:"+ suma)
+alert("El resultado en la resta es:"+ resta)
+console.log("El resultado en la multiplicacion es:" + multiplicacion)
+console.log("El resultado en la division es:" + division)
+let nombreAlumno = "Zinedine"
+let apellidoAlumno = "Zidane"
+let edadAlumno = "53"
+console.log("El alumno: "+nombreAlumno+" "+apellidoAlumno+" "+"tiene "+edadAlumno+" "+"años")
+let precio1 = 67
+let precio2 = 61
+let precio3 = 41
+let total = (precio1+precio2+precio3)
+console.log("El total a pagar es:"+total)
+let comidaFavorita = "Fideos con tuco"
+let colorFavorito = "Rojo"
+let numeroFavorito = 15
+console.log("Mi comida favorita son los "+comidaFavorita+" me gusta el color "+colorFavorito+" y mi numero favorito es el "+numeroFavorito)
+let pais = "Argentina"
+console.log(pais)
+pais = "Brasil"
+console.log(pais)
