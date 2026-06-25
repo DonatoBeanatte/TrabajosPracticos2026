c9f752e5ada7e5a722e089ae9379f12f23f4381c
alumno <alumno@Instituto.local>
2026-06-25 09:02:20 -0300

    cambios.js 25/06

--- TP6/app.js
+++ TP6/app.js
@@ -1,9 +1,5 @@
-//Guardar objeto
-
-
-//Ejercicio1
-let titulo = document.querySelector('h1')
-let boton = document.querySelector('button')
-
-boton onclick = function
-titulo.textcontent = 
+//Variables
+let title = document.querySelector ('h1')
+let parrafo = document.querySelector ('p')
+let BotonCambiarFondo = document.querySelector (CyanColorFondo)
+let BotonCambiarTexto = document.querySelector ()
