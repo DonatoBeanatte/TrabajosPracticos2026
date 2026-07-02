abfb9f78a3b4e1461a35a6b34cb74cfbcaf6593a
Dona_01 <52212725@ivt.edu.ar>
2026-07-01 22:47:49 -0300

    cambios.js

--- TP6/app.js
+++ TP6/app.js
@@ -1,5 +1,31 @@
 //Variables
-let title = document.querySelector ('h1')
-let parrafo = document.querySelector ('p')
-let BotonCambiarFondo = document.querySelector (CyanColorFondo)
-let BotonCambiarTexto = document.querySelector ()
+let btnTexto = document.querySelector('BotonCambiarTexto')
+let btnColor = document.querySelector('BotonCambiarColor')
+let btnFondo = document.querySelector('BotonCambiarFondo')
+let btnLetra = document.querySelector('BotonCambiarLetra')
+let btnTamaño = document.querySelector('BotonCambiarTamaño')
+let btnReiniciar = document.querySelector('BotonReiniciar')
+
+let p = document.querySelector('p')
+btnTexto.onclick = function(){
+    p.textContent = 'Bienvenido a programación'
+}
+btnColor.onclick = function (){
+    p.style.color = 'green'
+}
+btnFondo.onclick = function(){
+    p.style.backgroundColor = 'purple'
+}
+btnLetra.onclick = function(){
+    p.style.fontFamily = 'IMPACT'
+}
+btnTamaño.onclick = function(){
+    p.style.fontSize = '30px'
+}
+btnReiniciar.onclick = function(){
+    p.textContent = 'Bienvenido al TP6'
+    p.style.backgroundColor = 'rgb(80, 87, 87)'
+    p.style.color = 'rgb(4, 4, 5)'
+    p.style.fontFamily = 'Arial, Helvetica, sans-serif'
+    p.style.fontSize = '15px'
+}
