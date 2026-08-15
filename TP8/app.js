2e87a3c9dca002b5a5dcdf2ff090bf823983c91b
Dona_01 <52212725@ivt.edu.ar>
2026-08-14 23:18:43 -0300

    cambios.js

---
+++ TP8/app.js
@@ -0,0 +1,7 @@
+let Ej1 = document.querySelector('#Ej1')
+let botonEj1 = document.querySelector('#botonEj1')
+let inputEj1 = document.querySelector('#inputEj1')
+let edad = 14
+botonEj1.onclick = function(){
+    edad = inputEj1.value}
+    if
